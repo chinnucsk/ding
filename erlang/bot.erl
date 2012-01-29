8f1c8ea4587f892a4f506c36e7b77325e9087c1b
Gert Meulyzer <cel357@gmail.com>
2012-01-29 10:07:39 +0100

    cleaned up the erlang code a bit

--- erlang/bot.erl
+++ erlang/bot.erl
@@ -9,34 +9,25 @@
 
 -behaviour(girc).
 -include("irc.hrl").
--compile(export_all).
--export([start/0, handle_msg/1, handle_disconnect/0]).
+
+-export([start/0, handle_msg/1, check_for_url/1]).
 
 start() ->
     inets:start(),
-    girc:start_link(?MODULE, "irc.freenode.net", 6667, "yflbot").
+    girc:start_link(?MODULE, "irc.quakenet.org", 6667, "botje").
 
-
-handle_msg(#ircmsg{prefix=_P, command=C, arguments=_A, tail=T}=Msg) ->
-    case C of
-        <<"PING">> ->
-            io:format("Replying with PONG ~p~n",[T]),
-            #ircmsg{command="PONG",
-                    tail=T};
-        <<"002">> ->
-            #ircmsg{command="JOIN", arguments=["#yfl"]};
-        <<"PRIVMSG">> ->
-            handle_privmsg(Msg);
-        _ -> ok
-    end.
-
-handle_disconnect() ->
-    io:format("DISCONNECTED!~n"),
-    irc_connection:connect(?MODULE, "irc.freenode.net", 6667, "yflbot"),
+handle_msg(#ircmsg{command = <<"PRIVMSG">>}=Msg) ->
+    handle_privmsg(Msg);
+handle_msg(#ircmsg{command = <<"PING">>, tail=T}) ->
+    #ircmsg{command="PONG", tail=T};
+handle_msg(#ircmsg{command = <<"002">>}) ->
+    #ircmsg{command="JOIN", arguments=["#euronarp"]};
+handle_msg(#ircmsg{prefix=_P, command=_C, arguments=_A, tail=_T}=_Msg) ->
     ok.
 
 -spec handle_privmsg(Msg :: #ircmsg{}) -> #ircmsg{} | ok.
-handle_privmsg(#ircmsg{prefix=_P, command=_C, arguments=A, tail=T}=_Msg) ->
+handle_privmsg(#ircmsg{prefix=_P, command=_C, arguments=A, tail=T}=Msg) ->
+	io:format("~p~n",[Msg]),
     case A of
         [<<"#euronarp">>] ->
             parse_gt_command(T);
@@ -45,14 +36,14 @@ handle_privmsg(#ircmsg{prefix=_P, command=_C, arguments=A, tail=T}=_Msg) ->
 
 parse_gt_command(Line) ->
     if byte_size(Line) > 2 ->
-            case binary:part(Line,0,3) of
+            case binary:part(Line, 0, 3) of
                 <<"!GT">> ->
-                    UserName = binary:replace(Line,<<"!GT ">>,<<"">>),
+                    UserName = binary:replace(Line, <<"!GT ">>, <<"">>),
                     Status = (catch get_xbox_info_for_username(UserName)),
                     Reply = case Status of
-                                {'EXIT',_} -> "Error!";
+                                {'EXIT', _} -> "Error!";
                                 _ -> Status
-                            end, 
+                            end,
                     #ircmsg{command="PRIVMSG",
                             arguments=["#euronarp"],
                             tail=Reply};
@@ -61,17 +52,29 @@ parse_gt_command(Line) ->
        true -> ok
     end.
 
+get_http_contents(Url) ->
+	{ok, {_, _, Contents}} = httpc:request(Url),
+	Contents.
+
 get_xbox_info_for_username(Username) ->
+    %% http://api.xboxleaders.com/v2/?gamertag=GAMERTAG&format=json
     JsonURL = "http://api.xboxleaders.com/v2/?gamertag="++binary:bin_to_list(Username)++"&format=json",
-    {ok, {_,_,RawJson}} = httpc:request(JsonURL),
+    {ok, {_, _, RawJson}} = httpc:request(JsonURL),
     Json = mochijson:decode(RawJson),
     {struct, [User]} = Json,
-    {"user",{struct, Props}} = User,
+    {"user", {struct, Props}} = User,
     GamerScore = proplists:get_value("gamerscore", Props),
     OnlineStatus = proplists:get_value("online_status", Props),
     io_lib:format("~s (GS: ~p)", [OnlineStatus, GamerScore]).
 
-    
+check_for_url(Line) ->
+	Pattern="(http|ftp|https):\\/\\/[\\w\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?",
+	{ok, Regex} = re:compile(Pattern,[caseless]),
+	case re:run(Line, Regex, [{capture, first, binary}]) of
+		{match, [H]} -> H;
+		_ -> []
+	end.
 
-
-
+tinyurl(Url) ->
+	%% http://tinyurl.com/api-create.php?url=http://scripting.com/
+	undefined.
--- erlang/girc.erl
+++ erlang/girc.erl
@@ -7,10 +7,11 @@
 %%% Created : 28 Dec 2011 by Gert Meulyzer <@G3rtm on Twitter>
 %%%-------------------------------------------------------------------
 -module(girc).
--compile(export_all).
 
 -behaviour(gen_server).
 -include("irc.hrl").
+-define(COLON, 58).
+
 %% API
 -export([start_link/4]).
 
@@ -18,9 +19,9 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--export([send_msg/2, send_raw/2]).
+-export([send_msg/2, send_raw/2, is_CTCP/1]).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
 
 -record(state, {socket, host, port=6667, username, callbackmodule}).
 
@@ -29,6 +30,12 @@
 %%%===================================================================
 %%% API
 %%%===================================================================
+
+send_msg(Pid, #ircmsg{}=Msg) ->
+    gen_server:cast(Pid, {send_msg, Msg}).
+send_raw(Pid, Line) ->
+    gen_server:cast(Pid, {send_raw, Line}).
+
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -56,14 +63,7 @@ start_link(Module, Host, Port, UserName) ->
 %% @end
 %%--------------------------------------------------------------------
 init([Module, Host, Port, UserName]) ->
-    {ok, Sock} = gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}]),
-    {ok, _} = gen_tcp:recv(Sock, 0),
-    inet:setopts(Sock, [{active, true}]),
-    gen_tcp:send(Sock, "USER "++UserName++" "++UserName++" "++UserName++" "++UserName),
-    gen_tcp:send(Sock, "\r\n"),
-    gen_tcp:send(Sock, "NICK "++UserName),
-    gen_tcp:send(Sock, "\r\n"),
-    {ok, #state{socket=Sock, host=Host, port=Port, username=UserName, callbackmodule=Module}}.
+	{ok, #state{host=Host, port=Port, username=UserName, callbackmodule=Module}, 0}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -104,12 +104,6 @@ handle_cast(stop, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-%% wrappers
-send_msg(Pid, #ircmsg{}=Msg) ->
-    gen_server:cast(Pid, {send_msg, Msg}).
-send_raw(Pid, Line) ->
-    gen_server:cast(Pid, {send_raw, Line}).
-
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -120,31 +114,29 @@ send_raw(Pid, Line) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
-handle_info({tcp, _S, Data}, #state{socket=Sock,callbackmodule=Mod}=State) ->
+handle_info(timeout, #state{host=Host, port=Port, username=UserName, callbackmodule=Module}) ->
+    {ok, Sock} = gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, false}]),
+    {ok, _} = gen_tcp:recv(Sock, 0),
+    inet:setopts(Sock, [{active, true}]),
+    %% Do the IRC login
+    gen_tcp:send(Sock, "USER "++UserName++" "++UserName++" "++UserName++" "++UserName),
+    gen_tcp:send(Sock, "\r\n"),
+    gen_tcp:send(Sock, "NICK "++UserName),
+    gen_tcp:send(Sock, "\r\n"),
+    {noreply, #state{socket=Sock, host=Host, port=Port, username=UserName, callbackmodule=Module}};
+handle_info({tcp, _S, Data}, #state{socket=Sock, callbackmodule=Mod}=State) ->
     Lines = lines(Data),
-    Msgs = lists:map(fun(X) ->
-                             R = (catch parse_line(X)),
-                             case R of
-                                 {'EXIT',_} -> #ircmsg{command = <<"NOOP">>};
-                                 _ -> R
-                             end
-                     end, Lines),
-    io:format("~p~n", [Msgs]),
-    Responses = lists:map(fun Mod:handle_msg/1, Msgs),
-    lists:foreach(fun(R) ->
-                          case R of
-                              ok -> ok;
-                              [_|_] -> 
-                                  lists:foreach(fun(X) -> send_ircmsg(Sock, X) end, R);
-                              _ -> send_ircmsg(Sock, R)
-                          end
-                  end,
-                  Responses),
+	Msgs = [ parse_line(X) || X <- Lines],
+	Responses = lists:flatten([ Mod:handle_msg(Msg) || Msg <- Msgs ]),
+	[ send_ircmsg(Sock, R) || R <- Responses ],
     {noreply, State};
 handle_info({tcp_closed, _Port}, State) ->
+	io:format("DISCONNECTED!!!!"),
+	%% we really should do something here to reconnect?
+	%% or let the supervisor do that later?
     {stop, disconnected, State};
 handle_info(Info, State) ->
-    io:format("UNKNOWN: ~p~n",[Info]),
+    io:format("UNKNOWN: ~p~n", [Info]),
     {noreply, State}.
 
 %%--------------------------------------------------------------------
@@ -177,31 +169,27 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 lines(Packet) ->
-    lists:filter(fun(X) -> not(X =:= <<>>) end,
-                 binary:split(Packet, <<"\r\n">>)).
+	binary:split(Packet, <<"\r\n">>, [global, trim]).
 
+starts_with_colon([]) ->
+	false;
+starts_with_colon(<<>>) ->
+	false;
 starts_with_colon(Bin) ->
-    case Bin of
-        [] -> false;
-        <<>> -> false;
-        _ -> binary:first(Bin) == 58
-    end.
+	binary:first(Bin) == ?COLON.
 
+next_word2([]) ->
+	{[], []};
 next_word2(Bin) ->
-    case Bin of
-        [] -> {[], []};
-        _ ->
-            case binary:split(Bin, <<" ">>) of
-                [H] -> {H, []};
-                [H|T] -> {H, hd(T)}
-            end
-    end.
+	case binary:split(Bin, <<" ">>) of
+		[H] -> {H, []};
+		[H|T] -> {H, hd(T)}
+	end.
 
+get_words_before_colon(<<>>) ->
+	{[], <<>>};
 get_words_before_colon(Bin) ->
-    case Bin of
-        <<>> -> {[], Bin};
-        _ -> get_another_word(Bin, [])
-    end.
+	get_another_word(Bin, []).
 
 get_another_word(Bin, Words) ->
     case starts_with_colon(Bin) of
@@ -217,54 +205,52 @@ get_another_word(Bin, Words) ->
 without_colon(Bin) ->
     hd(tl(binary:split(Bin, <<":">>))).
 
+parse_line(<<>>) ->
+	#ircmsg{};
 parse_line(IrcBinLine) ->
-    case IrcBinLine of
-        <<>> -> #ircmsg{};
-        _ ->
-            {Prefix, CommandsAnd} =
-                case starts_with_colon(IrcBinLine) of
-                    true ->
-                        {P, C} = next_word2(IrcBinLine),
-                        {without_colon(P), C};
-                    false ->
-                        {undefined, IrcBinLine}
-                end,
-            {Command, ArgsAnd} = next_word2(CommandsAnd),
-            {Args, T} = get_words_before_colon(ArgsAnd),
-            Tail = case T of
-                       <<>> -> <<>>;
-                       _ -> without_colon(T)
-                   end,
-            #ircmsg{prefix=Prefix, command=Command, arguments=Args, tail=Tail}
-    end.
+	{Prefix, CommandsAnd} =
+		case starts_with_colon(IrcBinLine) of
+			true ->
+				{P, C} = next_word2(IrcBinLine),
+				{without_colon(P), C};
+			false ->
+				{undefined, IrcBinLine}
+		end,
+	{Command, ArgsAnd} = next_word2(CommandsAnd),
+	{Args, T} = get_words_before_colon(ArgsAnd),
+	Tail = case T of
+			   <<>> -> <<>>;
+			   _ -> without_colon(T)
+		   end,
+	#ircmsg{prefix=Prefix, command=Command, arguments=Args, tail=Tail}.
 
-send_ircmsg(Sock, #ircmsg{prefix=P,command=C, arguments=A, tail=T}) ->
-    case P of
-        undefined -> ok;
-        _ -> gen_tcp:send(Sock,":"),
-             gen_tcp:send(Sock, P),
-             gen_tcp:send(Sock, " ")
-    end,
-    gen_tcp:send(Sock, C),
-    gen_tcp:send(Sock, " "),
-    case A of
-        [] -> ok;
-        [[]] -> ok;
-        _ -> gen_tcp:send(Sock, string:join(A," ")),
-             gen_tcp:send(Sock, " ")
-    end,
-    case T of
-        <<>> -> ok;
-        [] -> ok;
-        undefined -> ok;
-        _ -> gen_tcp:send(Sock, ":"),
-             gen_tcp:send(Sock, T)
-    end,
-    gen_tcp:send(Sock,"\r\n").
+send_ircmsg(_Sock, ok) ->
+	ok;
+send_ircmsg(Sock, #ircmsg{prefix=P, command=C, arguments=A, tail=T}) ->
+    Reply = [case P of
+				 undefined -> [];
+				 _ -> [":",P," "]
+			 end,
+			 [C," "],
+			 case A of
+				 [] -> [];
+				 [[]] -> [];
+				 _ -> [string:join(A, " "), " "]
+			 end,
+			 case T of
+				 <<>> -> [];
+				 [] -> [];
+				 undefined -> [];
+				 _ -> [":", T]
+			 end,
+			 "\r\n"],
+    gen_tcp:send(Sock, Reply).
 
 send_rawmsg(Sock, Line) ->
-    gen_tcp:send(Sock, Line),
-    gen_tcp:send(Sock, "\r\n").
+    gen_tcp:send(Sock, [Line, "\r\n"]).
+
+is_CTCP(#ircmsg{tail=T}) ->
+	binary:first(T) == 1 andalso binary:last(T) == 1.
 
 test_it() ->
     io:format("~p~n", [parse_line(<<":some.prefix.of.the.server PRIVMSG #testchannel :this is the text">>)]),
