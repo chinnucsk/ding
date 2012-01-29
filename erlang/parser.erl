1bbd3166125079eaeb7eced67242a99b99a5a7ab
Gert Meulyzer <cel357@gmail.com>
2012-01-29 22:58:20 +0100

    attempt at writing another parser

--- erlang/parser.erl
+++ erlang/parser.erl
@@ -27,3 +27,37 @@ starts_with_colon(<<>>) ->
 starts_with_colon(Bin) ->
     binary:first(Bin) == ?COLON.
 
+-spec words_in_line(Line :: binary()) -> [binary()].
+words_in_line(Line) ->
+    binary:split(Line, <<" ">>, [global, trim]).
+
+next_word([]) ->
+    {the_end, []};
+next_word([H]) ->
+    {H, []};
+next_word([H|T]) ->
+    {H, T}.
+
+-spec get_prefix(Words :: [binary()]) -> {binary(), [binary()]}.
+get_prefix([H|T]=L) ->
+    case starts_with_colon(H) of
+        true -> {H, T};
+        _ -> {<<>>, L}
+    end;
+get_prefix(A) ->
+    {<<>>,A}.
+
+-spec get_arguments_and_tail(AfterCmd :: [binary()]) -> {binary(), [binary()]}.
+get_arguments_and_tail(AfterCmd) ->
+    {Args, T} = lists:splitwith(fun(X) -> not(starts_with_colon(X)) end, AfterCmd),
+    {Args, iolist_to_binary([ [ <<" ">>, X ] || X <- T ])}.
+
+
+-spec parse_line(Line :: binary()) -> [tuple()].
+parse_line(Line) ->
+    Words = words_in_line(Line),
+    {P, Rest1} = get_prefix(Words),
+    {C, Rest2} = next_word(Rest1),
+    {A, T} = get_arguments_and_tail(Rest2),
+    [{prefix, P}, {cmd, C}, {args, A}, {tail, T}].
+
