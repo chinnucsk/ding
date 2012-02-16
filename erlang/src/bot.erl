a587f333c01285d70e8d2cce8833f968a84b9a07
Gert Meulyzer <cel357@gmail.com>
2012-02-16 09:07:07 +0100

    Removed some unused stuff.

--- erlang/src/bot.erl
+++ erlang/src/bot.erl
@@ -13,33 +13,19 @@
 
 start() ->
     inets:start(),
-    girc:start_link(?MODULE, "irc.freenode.net", 6667, "yflbot").
+    girc:start_link(?MODULE, "irc.freenode.net", 6667, "dingbot").
 
 handle_msg(Msg) ->
     case ircmsg:command(Msg) of
         <<"PRIVMSG">> -> handle_privmsg(Msg);
         <<"PING">> -> ircmsg:create(<<>>,<<"PONG">>,[],ircmsg:tail(Msg));
-        <<"002">> -> ircmsg:create(<<>>,<<"JOIN">>,[<<"#yfl">>],<<>>); %% this is a little crude, it won't work on all IRC servers.
+        <<"002">> -> ircmsg:create(<<>>,<<"JOIN">>,[<<"#erlounge">>],<<>>); %% this is a little crude, it won't work on all IRC servers.
         _ -> none
     end.
 
 -spec handle_privmsg(Msg :: ircmsg:ircmsg()) -> ircmsg:ircmsg() | ok.
 handle_privmsg(Msg) ->
     io:format("~p~n",[Msg]).
-
-get_http_contents(Url) ->
-    {ok, {_, _, Contents}} = httpc:request(Url),
-    Contents.
-
-get_xbox_info_for_username(Username) ->
-    JsonURL = "http://api.xboxleaders.com/v2/?gamertag="++binary:bin_to_list(Username)++"&format=json",
-    {ok, {_, _, RawJson}} = httpc:request(JsonURL),
-    Json = mochijson:decode(RawJson),
-    {struct, [User]} = Json,
-    {"user", {struct, Props}} = User,
-    GamerScore = proplists:get_value("gamerscore", Props),
-    OnlineStatus = proplists:get_value("online_status", Props),
-    io_lib:format("~s (GS: ~p)", [OnlineStatus, GamerScore]).
 
 check_for_url(Line) ->
     Pattern="(http|ftp|https):\\/\\/[\\w\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?",
